eb346e0a78a00acd1969a150b4a1cf1bae39bcd3
张俊超 <18235757166@163.com>
2020-03-23 14:55:11 +0800

    Initial commit

---
+++ src/main/java/com/atguigu/springboot/bean/Student.java
@@ -0,0 +1,39 @@
+package com.atguigu.springboot.bean;
+
+public class Student {
+
+    public String name ;
+    public Integer age ;
+
+    public Student() {
+    }
+
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
